0384e1f759bbf44c15195f2e3bc3bb56c303c99d
jakubGryzio <jakubgryzio@gmail.com>
2022-06-05 20:51:07 +0200

    Linking ifc project components logic

--- src/Storage/Neo4jHandler.py
+++ src/Storage/Neo4jHandler.py
@@ -13,7 +13,7 @@ class Neo4jHandler:
         driver = GraphDatabase.driver(self.HOST, auth=self.AUTH)
         return driver.session()
 
-    def load_csv(self, csv_path):
+    def load_csv_queries(self, csv_path):
         with open(csv_path, 'rt', encoding='UTF-8') as f:
             reader = csv.reader(f)
             for idx, line in enumerate(reader):
@@ -23,3 +23,33 @@ class Neo4jHandler:
                     print(f'----- DONE {idx + 1} query -----')
                 except Exception as e:
                     print(e)
+
+    def select_node_by_label(self, label):
+        query = "MATCH (node {{label: '{}'}}) RETURN node.id, node.attributes".format(label)
+        return self.session.run(query)
+
+    def select_node_by_id(self, id):
+        query = "MATCH (node {{id: '{}'}}) RETURN node".format(id)
+        return self.session.run(query)
+
+    def select_node_by_id_in(self, ids):
+        query = "MATCH (node) WHERE node.id IN [{}] RETURN node".format(ids)
+        return self.session.run(query)
+
+    def create_relationships(self):
+        FIRST_SEPARATOR = '('
+        SECOND_SEPARATOR = ','
+        STRIP_ELEMENT = ')'
+        LABEL = 'IFCRELAGGREGATES'
+        result = self.select_node_by_label(LABEL)
+        for node in result:
+            attributes = node[1].rsplit(FIRST_SEPARATOR)
+            relatingId = attributes[0].split(SECOND_SEPARATOR)[-2]
+            split_relatingId_elements = attributes[-1].rstrip(STRIP_ELEMENT).split(SECOND_SEPARATOR)
+            relatedId = SECOND_SEPARATOR.join("'" + elem + "'" for elem in split_relatingId_elements)
+            relating_query = "MATCH (r:ifcID {{id: '{}'}}), (n:ifcID {{id: '{}'}}) CREATE (r)-[:RELATING]->(n);".format(node[0], relatingId)
+            related_query = "MATCH (r:ifcID {{id: '{}'}}), (n:ifcID) WHERE n.id IN [{}] CREATE (r)-[:RELATED]->(n);".format(node[0], relatedId)
+            self.session.run(relating_query)
+            self.session.run(related_query)
+            print(node)
+
