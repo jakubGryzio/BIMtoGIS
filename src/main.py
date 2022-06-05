055b45ad390d1c32d9d69bcf1a9174d58ab4ed39
jakubGryzio <jakubgryzio@gmail.com>
2022-06-05 20:54:58 +0200

    Builded main functionality

--- src/main.py
+++ src/main.py
@@ -1,12 +1,21 @@
-from src.Loader.Loader import Loader
-from src.Storage.Neo4jHandler import Neo4jHandler
+from Loader.Loader import Loader
+from Storage.Neo4jHandler import Neo4jHandler
 
 FILEPATH = '../model/AC20-Institute-Var-2.ifc'
 CSV_PATH = "./ifc_objects.csv"
 
-loader = Loader()
-loader.file_loader(FILEPATH)
-# loader.ifc_objects_provider(CSV_PATH)
+def load_ifc_file():
+    loader = Loader()
+    loader.file_loader(FILEPATH)
+    loader.ifc_objects_provider(CSV_PATH)
 
-neo = Neo4jHandler()
-neo.load_csv(CSV_PATH)
+def import_ifc_object():
+    neo = Neo4jHandler()
+    neo.load_csv_queries(CSV_PATH)
+
+def neo4j_handler():
+    neo = Neo4jHandler()
+    neo.create_relationships()
+
+if __name__ == '__main__':
+    neo4j_handler()
