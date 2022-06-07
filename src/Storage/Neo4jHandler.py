849dd232e63b8775020976b52ffbd42743c2021a
jakubGryzio <jakubgryzio@gmail.com>
2022-06-07 18:42:07 +0200

    TrueNorth and project insertion point logic

--- src/Storage/Neo4jHandler.py
+++ src/Storage/Neo4jHandler.py
@@ -8,6 +8,8 @@ class Neo4jHandler:
 
     def __init__(self):
         self.session = self.connect()
+        self.trueNorth = self.trueNorthInfo()
+        self.cartPoint = self.insertionCoordSystemPoint()
 
     @staticmethod
     def extract_attribute(node, separator):
@@ -31,6 +33,10 @@ class Neo4jHandler:
 
     def select_node_by_label(self, label):
         query = "MATCH (node {{label: '{}'}}) RETURN node.id, node.label, node.attributes".format(label)
+        return self.session.run(query)
+
+    def select_node_by_label_with_limit(self, label, limit):
+        query = "MATCH (node {{label: '{}'}}) RETURN node.id, node.label, node.attributes LIMIT {}".format(label, limit)
         return self.session.run(query)
 
     def create_project_relationship(self):
@@ -132,4 +138,32 @@ class Neo4jHandler:
 
     def create_geometry_relationship(self):
         pass
+
+    def trueNorthInfo(self):
+        FIRST_SEPARATOR = '('
+        SECOND_SEPARATOR = ','
+        LABEL = 'IFCGEOMETRICREPRESENTATIONCONTEXT'
+        result = self.select_node_by_label_with_limit(LABEL, 1)
+        for node in result:
+            attributes = node[2].rsplit(FIRST_SEPARATOR)
+            trueNorthId = attributes[-1].split(SECOND_SEPARATOR)[-1]
+            trueNorth_query = "MATCH (n:ifcID {{id: '{}'}}) RETURN n.attributes".format(trueNorthId)
+            result = [attr[0] for attr in self.session.run(trueNorth_query)]
+            return result[0]
+
+    def insertionCoordSystemPoint(self):
+        FIRST_SEPARATOR = '('
+        SECOND_SEPARATOR = ','
+        LABEL = 'IFCGEOMETRICREPRESENTATIONCONTEXT'
+        result = self.select_node_by_label_with_limit(LABEL, 1)
+        for node in result:
+            attributes_kp = node[2].rsplit(FIRST_SEPARATOR)
+            AXIS2PLACEMENT3DId = attributes_kp[-1].split(SECOND_SEPARATOR)[-2]
+            AXIS2PLACEMENT3DId_query = "MATCH (n:ifcID {{id: '{}'}}) RETURN n.attributes".format(AXIS2PLACEMENT3DId)
+            result_axis_placement = [attr[0] for attr in self.session.run(AXIS2PLACEMENT3DId_query)]
+            cartPointId = result_axis_placement[0].split(SECOND_SEPARATOR)[0]
+            cartPoint_query = "MATCH (n:ifcID {{id: '{}'}}) RETURN n.attributes".format(cartPointId)
+            result_cartPoint = [attr[0] for attr in self.session.run(cartPoint_query)]
+            cartPoint = result_cartPoint[0]
+            return cartPoint
 
